ddd35a7dba531805d3419982929ab40b26fc1c2c
hemangdevaliya <devaliyahemang@gmail.com>
2025-06-15 13:01:57 +0530

    Initial commit: Django Channels Chat App

---
+++ chatproject/chatproject/asgi.py
@@ -0,0 +1,31 @@
+"""
+ASGI config for chatproject project.
+
+It exposes the ASGI callable as a module-level variable named ``application``.
+
+For more information on this file, see
+https://docs.djangoproject.com/en/4.2/howto/deployment/asgi/
+"""
+
+import os
+import django
+
+
+from channels.routing import ProtocolTypeRouter,URLRouter
+from channels.auth import AuthMiddlewareStack
+from django.core.asgi import get_asgi_application
+
+from chat.routing import websocket_urlpatterns
+
+os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'chatproject.settings')
+django.setup()
+
+# application = get_asgi_application()
+application = ProtocolTypeRouter({
+    "http": get_asgi_application(),
+    "websocket": AuthMiddlewareStack(
+        URLRouter(
+            websocket_urlpatterns
+        )
+    ),
+})
